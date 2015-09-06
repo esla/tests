c841704a30e9386137bbe6bd7ac890b8a4be5874
esla <timothy.anzaku@linaro.org>
2015-09-07 02:11:12 +0900

    modified ltpddt.sh

--- common/scripts/ltpddt.sh
+++ common/scripts/ltpddt.sh
@@ -70,7 +70,7 @@ main()
         echo "WARNING: Will run all ltp-ddt testcases or all those that match PATTERNS"
     fi
 
-    if [ -n "$PATTERN" ]; then
+    if [ -n "$PATTERNS" ]; then
         PATTERNS_OPTION = "-s $PATTERNS"
     fi
 
