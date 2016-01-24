74c4e5f886c1f77bd852e1f03f393a6ec8d586fc
Esla <slacowr@gmail.com>
2016-01-24 05:04:18 +0100

    Add printing to stdout in test_python.py

--- openembedded/scripts/test_python.py
+++ openembedded/scripts/test_python.py
@@ -3,10 +3,14 @@
 import subprocess
 
 print "Running a simple test"
-wget_command = "wget http://people.linaro.org/~timothy.anzaku/bb_black/zImage"
-ret = subprocess.call(wget_command,shell=True)
+#wget_command = "wget http://people.linaro.org/~timothy.anzaku/bb_black/zImage"
+#ret = subprocess.call(wget_command,shell=True)
 
-if ret == 0:
-	print "test: pass"
-else:
-	print "test: fail"
+#if ret == 0:
+#	print "test: pass"
+#else:
+#	print "test: fail"
+
+f = open('output_test.log', 'r')
+print f.read()
+f.close()
