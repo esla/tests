92319daf6b90c852b3f3e9ea444635bb455fb269
Esla <slacowr@gmail.com>
2016-02-23 00:14:32 +0900

    Change to logfile

--- openembedded/scripts/test_python.py
+++ openembedded/scripts/test_python.py
@@ -20,8 +20,8 @@ print "Running a simple test"
 # Second Attempt -- Run similar commands as in the 
 # first case but in a subprocess
 
-#with open('logfile') as file:
-with open('logfile_without_CR') as file:
+with open('logfile') as file:
+#with open('logfile_without_CR') as file:
     header = file.readline()
     rc = subprocess.call(['cat'], stdin=file)
 file.close()
