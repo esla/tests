c1a57a307453dc098387e35d176fb19a38ed3bae
Esla <slacowr@gmail.com>
2016-03-17 13:36:46 +0900

    Edit

--- openembedded/scripts/test_python.py
+++ openembedded/scripts/test_python.py
@@ -30,7 +30,7 @@ import textwrap
 f= open('logfile','r')
 #for line in f.read().split('\r'):
 for line in f.readlines():
-	print "\n".join(textwrap.wrap(line, width=80))
+	print "\n".join(textwrap.wrap(line, width=2000))
 f.close()
 
 print "End of test"
